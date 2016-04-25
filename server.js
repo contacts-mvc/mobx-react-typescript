b1ce02bcedd5fba08c82dc543b94ed91e79ccfee
Mohsen Azimi <mazimi@lyft.com>
2016-04-25 14:40:23 -0700

    Listen to process.env.PORT

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var PORT = process.env.PORT || 3000;
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   quiet: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(PORT, function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://localhost:' + PORT);
 });
